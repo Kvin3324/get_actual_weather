ea1f8c9134f913c29f23c81d8ea6092cacb6d076
Kévin <kevin.joya@outlook.com>
2021-09-21 16:01:09 +0200

    Created compo

---
+++ src/components/ActualWeatherData.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="weather__data__actual">
+    <div class="weather__data__actual--city">
+      <h4> {{ city }}, {{ country }}</h4>
+    </div>
+    <div class="weather__data__actual--temp">
+      <img :src="`https://openweathermap.org/img/w/${weatherIcon}.png`" />
+      <div class="weather__data__actual--temp--about">
+        <h4>{{ weatherTemp }}°</h4>
+        <p class="weather--description">{{ weatherDescription }}</p>
+        <p class="weather--date">{{ weatherDateDay }} {{ weatherDateMonth }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'actual-weather-data',
+  props: {
+    city: {
+      type: String,
+      default: ''
+    },
+    country: {
+      type: String,
+      default: ''
+    },
+    weatherIcon: {
+      type: String,
+      default: ''
+    },
+    weatherTemp: {
+      type: Number,
+      default: 0
+    },
+    weatherDescription: {
+      type: String,
+      default: ''
+    },
+    weatherDateDay: {
+      type: String,
+      default: ''
+    },
+    weatherDateMonth: {
+      type: Number,
+      default: 0
+    },
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+  .weather__data__actual {
+    display: flex;
+    margin-top: 10px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    img {
+      margin-right: 10px;
+      width: inherit;
+      max-width: 150px;
+    }
+
+    &--temp {
+      width: 100%;
+      &--about {
+        h4 {
+          font-size: 4em;
+          margin: 0;
+          margin-left: 20px;
+        }
+        p {
+          margin: 0;
+        }
+        .weather--description {
+          font-weight: bold;
+        }
+        .weather--date {
+          opacity: .5;
+          font-size: .9em;
+        }
+      }
+    }
+
+    &--city {
+      display: flex;
+      flex-direction: column-reverse;
+
+      h4 {
+        font-weight: bold;
+      }
+    }
+
+    @media screen and (min-width: 320px) {
+      &--temp {
+        width: 95%;
+      }
+    }
+
+    @media screen and (min-width: 576px) {
+      &--temp {
+        width: 92%;
+      }
+    }
+
+    @media screen and (min-width: 768px) {
+      &--city {
+        h4 {
+          font-size: 2.5em;
+        }
+      }
+
+      &--temp {
+        width: 70%;
+
+        &--about {
+          h4 {
+            font-size: 5.5em;
+          }
+          .weather--description {
+            font-size: 1.5em;
+          }
+          .weather--date {
+            font-size: 1.3em;
+          }
+        }
+      }
+    }
+    @media screen and (min-width: 992px) {
+      &--temp {
+        width: 60%;
+      }
+    }
+
+    @media screen and (min-width: 1400px) {
+      &--temp {
+        width: 50%;
+      }
+    }
+  }
+</style>
---
+++ src/components/WeatherDataAbout.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="weather__data__about">
+    <div class="weather__data__about--variants">
+      <p class="label--about">max/min</p>
+      <p class="temp--data">{{ getWeatherTempMax }}° / {{ getWeatherTempMin }}°</p>
+    </div>
+    <div class="weather__data__about--humidity">
+      <p class="label--about">Taux d'humidité</p>
+      <p class="humidity--data">{{ getWeatherHumidity }}%</p>
+    </div>
+    <div class="weather__data__about--wind">
+      <p class="label--about" >Vent</p>
+      <p class="wind--data">{{ getWeatherWind }}km/h</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'weather-data',
+  props: {
+    // Props to have weather details data
+
+    getWeatherTempMin: {
+      type: Number,
+      default: 0
+    },
+    getWeatherTempMax: {
+      type: Number,
+      default: 0
+    },
+    getWeatherHumidity: {
+      type: Number,
+      default: 0
+    },
+    getWeatherWind: {
+      type: Number,
+      default: 0
+    },
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+  .weather__data__about {
+    display: flex;
+    justify-content: space-between;
+    font-size: .8em;
+
+    p {
+      padding: 4px;
+      margin: 0;
+    }
+
+    &--variants, &--humidity, &--wind {
+      display: flex;
+      flex-direction: column-reverse;
+      line-height: .7;
+
+      .temp--data, .humidity--data, .wind--data {
+        font-weight: bold;
+      }
+
+      .label--about {
+        opacity: .6;
+      }
+    }
+
+    @media screen and (min-width: 768px) {
+      div {
+        font-size: 1.3em;
+      }
+    }
+
+    @media screen and (min-width: 992px) {
+      div {
+        font-size: 1.5em;
+      }
+    }
+  }
+</style>
